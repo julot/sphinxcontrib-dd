c82648d8b063a71fecbbc1430a2a2851ba322d08
Julot <andy.julot@gmail.com>
2017-03-09 16:10:38 +0700

    Add relationship tester

---
+++ tests/database_diagram.py
@@ -0,0 +1,33 @@
+from sphinxcontrib.dd.database_diagram import Relationship
+
+
+def test_relationship():
+    relations = {
+        'E1 |--| E2': (
+            '"E1" -> "E2" '
+            '[dir="both", arrowhead="nonetee", arrowtail="nonetee"]'
+        ),
+        'E1 ||--|| E2': (
+            '"E1" -> "E2" '
+            '[dir="both", arrowhead="noneteetee", arrowtail="noneteetee"]'
+        ),
+        'E1 |0--0| E2': (
+            '"E1" -> "E2" '
+            '[dir="both", arrowhead="noneteeodot", arrowtail="noneteeodot"]'
+        ),
+        'E1 >--< E2': (
+            '"E1" -> "E2" '
+            '[dir="both", arrowhead="crow", arrowtail="crow"]'
+        ),
+        'E1 >0--0< E2': (
+            '"E1" -> "E2" '
+            '[dir="both", arrowhead="crowodot", arrowtail="crowodot"]'
+        ),
+        'E1 >|--|< E2': (
+            '"E1" -> "E2" '
+            '[dir="both", arrowhead="crowtee", arrowtail="crowtee"]'
+        ),
+    }
+
+    for relation, dot in relations.items():
+        assert Relationship(relation).dot() == dot
